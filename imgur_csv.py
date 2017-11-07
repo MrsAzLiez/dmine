73be7a28c230582e58cbe14ae22fc9e3eee98449
MrsAzLiez <31959505+MrsAzLiez@users.noreply.github.com>
2017-11-07 21:39:38 +0800

    Add files via upload

---
+++ imgur_csv.py
@@ -0,0 +1,9 @@
+import csv
+import datetime
+
+with open('imgur.csv', 'a') as csv_file:
+	writer = csv.writer(csv_file)
+	
+	#The for loop
+	for post_id, score in data:
+		writer.writerow([post_id, score, datetime.now()])
---
+++ youtube_csv.py
@@ -0,0 +1,9 @@
+import csv
+import datetime
+
+with open('youtube.csv', 'a') as csv_file:
+	writer = csv.writer(csv_file)
+	
+	#The for loop
+	for post_id, score in data:
+		writer.writerow([post_id, score, datetime.now()])
