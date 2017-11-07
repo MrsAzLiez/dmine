d9fba2d356808bbb2e24287a41a82d5f3a8944de
MrsAzLiez <31959505+MrsAzLiez@users.noreply.github.com>
2017-11-07 21:38:07 +0800

    Update and rename csv-save to tweet_csv.py

---
+++ tweet_csv.py
@@ -0,0 +1,10 @@
+
+import csv
+import datetime
+
+with open('tweet.csv', 'a') as csv_file:
+	writer = csv.writer(csv_file)
+	
+	#The for loop
+	for post_id, score in data:
+		writer.writerow([post_id, score, datetime.now()])
